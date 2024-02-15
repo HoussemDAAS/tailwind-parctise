7c36a518904bca42adf1b7d1566df528e45eeaaa
HoussemDAAS <houssemdaas2@gmail.com>
2024-02-15 19:41:50 +0100

    preparing styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+      <section className="padding">Popular products</section>
+      <section className="padding">Super quality</section>
+      <section className="padding-x py10">Services </section>
+      <section className="padding">Special offers</section>
+      <section className="padding bg-pale-blue ">Costumer Review</section>
+      <section className="padding-x sm:py32 py-16 w-full">Subcribe</section>
+      <section className="padding bg-black padding-x padding-y pb-8">Footer</section>
+    </main>
+  );
 }
 
-export default App
+export default App;
